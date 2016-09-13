0999ac52792a579403927387d574bfe56830cf7e
Benjamin Kaufmann <kaufmann@cs.uni-potsdam.de>
2016-09-13 17:06:43 +0200

    Initial commit

---
+++ program_opts-config.cmake
@@ -0,0 +1,4 @@
+# program_opts-config.cmake - package configuration file
+
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/program_opts.cmake)
